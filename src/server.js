396795769b61f1a17e6956a4d63c323f3adbea37
luisgarces1 <72531010+luisgarces1@users.noreply.github.com>
2021-04-25 11:33:05 -0500

    cambio 1

--- src/server.js
+++ src/server.js
@@ -1,43 +1,22 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var helmet = require('helmet');
-var rateLimit = require('express-rate-limit');
-var authentication = require('./authentication');
+const express = require('express');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const authentication = require('./authentication');
 
 var apiLimiterLogin = rateLimit({
-    max: 10
+    max: 1000
 });
 
-var port = 3001;
+var port = 3000;
 
 var server = express();
-
-server.use(helmet());
-server.use(bodyParser());
-server.use('/', apiLimiterLogin);
-
-var usuarios = [];
 
 server.get('/', (req, res) => {
     res.send('Bienvenidos a mi api de express');
 });
 
-server.get('/users', (req, res) => {
-    const userverified = authentication.verifyUser(req, res, usuarios);
-    if(userverified) {
-        res.send(usuarios);   
-    }else {
-        res.send('Error: ah ocurrido un problema con el token');   
-    }   
-})
-
-
-
-function validateEmail(email) {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
 
 server.listen(port, () =>{
-    console.log(`servidor correindo en el puerto ${port}`);
+    console.log(`servidor corriendo en el puerto ${port}`);
 });
